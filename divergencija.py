67ae54f279b795ec2897a3d7fc09cf5fc2478e0d
Miske <misic.pavle@gmail.com>
2025-03-19 18:12:50 +0000

    Dodata login stranica ali bez backend podrske

--- divergencija.py
+++ divergencija.py
@@ -1,6 +1,4 @@
 
-# A very simple Flask Hello World app for you to get started with...
-
 from flask import Flask, redirect, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 
@@ -11,20 +9,32 @@ SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostnam
     username="Miske",
     password="jebosamtimamu17211",
     hostname="Miske.mysql.pythonanywhere-services.com",
-    databasename="Miske$words",
+    databasename="Miske$comments",
 )
 app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
 app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
-comments = []
 
-@app.route('/', methods=["GET","POST"])
+class Comment(db.Model):
+
+    __tablename__ = "comments"
+
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(4096))
+
+
+@app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "GET":
-        return render_template("main.html", comments=comments)
+        return render_template("main.html", comments=Comment.query.all())
 
-    comments.append(request.form["contents"])
+    comment = Comment(content=request.form["contents"])
+    db.session.add(comment)
+    db.session.commit()
     return redirect(url_for('index'))
 
+@app.route("/login/")
+def login():
+    return render_template("login_page.html")
